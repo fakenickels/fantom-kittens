f081023b88284fe6c360ef96b04c90e0c2f247c3
Marco Worms <marcogworms@gmail.com>
2021-09-22 19:39:50 -0300

    remove minigames

--- public/minigames/scripts/register-sw.js
+++
@@ -1,1 +0,0 @@
-'use strict';{window.C3_RegisterSW=async function C3_RegisterSW(){if(!navigator.serviceWorker)return;try{const reg=await navigator.serviceWorker.register("sw.js",{scope:"./"});console.info("Registered service worker on "+reg.scope)}catch(err){console.warn("Failed to register service worker: ",err)}}};
