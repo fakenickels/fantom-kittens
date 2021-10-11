b3e8b72b97a113b1fe9f37a68784fdbbb5e701ef
Gabriel Abreu <grsabreu@gmail.com>
2021-10-11 16:08:17 -0300

    feat(rkitten): farm and rKITTEN contract

---
+++ contracts/RKITTEN.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Simple ERC20 contract, we prefer to leave the rest of the funcionality in other contracts
+contract RKITTEN is ERC20 {
+    constructor() ERC20("Kitten Royalties", "rKITTEN") {
+        _mint(msg.sender, 420000 * 10 ** decimals());
+    }
+}
