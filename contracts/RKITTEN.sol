a1b9197666ca9cea27ec726a1e224970f9f5cf49
Gabriel Abreu <grsabreu@gmail.com>
2021-10-18 11:46:08 -0300

    chore: better legibility

--- contracts/RKITTEN.sol
+++ contracts/RKITTEN.sol
@@ -6,6 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 // Simple ERC20 contract, we prefer to leave the rest of the funcionality in other contracts
 contract RKITTEN is ERC20 {
     constructor() ERC20("Kitten Royalties", "rKITTEN") {
-        _mint(msg.sender, 420000 * 10 ** decimals());
+        _mint(msg.sender, 420_000 * 10 ** decimals());
     }
 }
