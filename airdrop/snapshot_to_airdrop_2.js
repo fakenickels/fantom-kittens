56dd39ecf72b5fe63c65c03b6ad5824fed862e07
Gabriel Abreu <grsabreu@gmail.com>
2021-10-25 21:09:00 -0300

    feat: update

--- airdrop/snapshot_to_airdrop_2.js
+++ airdrop/snapshot_to_airdrop_2.js
@@ -3,40 +3,34 @@ const path = require("path");
 const BigNumber = require("bignumber.js");
 
 // kittens that were in paintswap's escrow during snapshot
+const NON_ELIGIBLE = 26;
+const KITTENS_SUPPLY = 419 - NON_ELIGIBLE;
 const RKITTENS_AIRDROP_AMOUNT = 50_000;
+const RKITTEN_PER_KITTEN = new BigNumber(RKITTENS_AIRDROP_AMOUNT).dividedBy(
+  KITTENS_SUPPLY
+);
 
-const [, ...snapshot] = fs
-  .readFileSync(path.join(__dirname, "snapshop_1.csv"))
-  .toString()
-  .split("\n")
-  .map((line) => line.split(",")[0]);
-
-const [, ...snapshot_2] = fs
+const [head, ...snapshot] = fs
   .readFileSync(path.join(__dirname, "snapshot_2.csv"))
   .toString()
-  .split("\n")
-  .map((line) => line.split(","));
+  .split("\n");
 
-// excludes all wallets in snapshot_2 that are in snapshot_1
-const airdrop_2 = snapshot_2.filter(
-  ([wallet, ...rest]) => !snapshot.includes(wallet)
-);
+const finalAirdrop = snapshot.map((line) => {
+  const [wallet, kittensAmount] = line.split(",");
+  return [wallet.trim(), RKITTEN_PER_KITTEN.multipliedBy(kittensAmount), kittensAmount];
+});
 
-const kittensInSnapshot = airdrop_2.reduce(
-  (acc, [_, kittens]) => acc + Number(kittens),
-  0
-);
+// console.log("DEBUG\n FINAL SUM");
+// console.log(
+//   finalAirdrop.reduce((acc, [_, amount,]) => amount.plus(acc), new BigNumber(0)).toNumber()
+// );
+// console.log(
+//   "totalKittens",
+//   snapshot.map(line => line.split(",")).reduce((acc, [_, kittensAmount]) => acc + Number(kittensAmount), 0)
+// )
+// console.log("DEBUG");
 
-const RKITTEN_PER_KITTEN = new BigNumber(RKITTENS_AIRDROP_AMOUNT).dividedBy(
-  kittensInSnapshot
-);
-
-const finalFormat = airdrop_2
-  // ratio the airdrop is based on the total amount of rkittens
-  .map(([wallet, amount]) => [
-    wallet,
-    new BigNumber(amount).times(RKITTEN_PER_KITTEN),
-  ])
+const finalFormat = finalAirdrop
   .map(([wallet, amount]) => `${wallet},${amount.toNumber()}`)
   .join("\n");
 
