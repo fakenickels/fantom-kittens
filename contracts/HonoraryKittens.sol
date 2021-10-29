2d61259694c87c44de6ca15c5c81aa1d8d4f406a
Gabriel Abreu <grsabreu@gmail.com>
2021-10-29 09:10:43 -0300

    feat: access control

--- contracts/HonoraryKittens.sol
+++ contracts/HonoraryKittens.sol
@@ -5,22 +5,30 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract HonoraryKittens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, IERC2981 {
+contract HonoraryKittens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, IERC2981, AccessControl {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    address payable public defaultReceiver = payable(0xC748E6dE30222F4e9bC01812860FF005A82543E6);
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     mapping(uint256 => address payable) public royaltyReceivers;
     mapping(uint256 => string) public kittenURIs;
     uint256 public constant royaltiesPercentage = 5;
 
-    constructor() ERC721("HonoraryKittens", "HKITTENS") {}
+    constructor() ERC721("HonoraryKittens", "HKITTENS") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    function mintNew(address payable creator, string memory metadataUri) public onlyOwner returns (uint256 createdId) {
+    function grantRole(address addr) public onlyOwner {
+        _setupRole(MINTER_ROLE, addr);
+    }
+
+    function mintNew(address payable creator, string memory metadataUri) public onlyRole(MINTER_ROLE) returns (uint256 createdId) {
         uint256 id = _tokenIdCounter.current();
         _safeMint(msg.sender, _tokenIdCounter.current());
         
@@ -65,24 +73,10 @@ contract HonoraryKittens is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable,
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable, IERC165)
+        override(ERC721, ERC721Enumerable, IERC165, AccessControl)
         returns (bool)
     {
         return type(IERC2981).interfaceId == interfaceId || super.supportsInterface(interfaceId);
-    }
-
-    // IERC2981 - Royalities
-    /// @notice Getter function for _royaltiesReceiver
-    /// @return the address of the royalties recipient
-    function royaltiesReceiver() external view returns(address) {
-        return defaultReceiver;
-    }
-
-    /// @notice Changes the royalties' recipient address (in case rights are transferred for instance)
-    /// @param newRoyaltiesReceiver - address of the new royalties recipient
-    function setRoyaltiesReceiver(address payable newRoyaltiesReceiver) external onlyOwner {
-        require(newRoyaltiesReceiver != defaultReceiver); // dev: Same address
-        defaultReceiver = newRoyaltiesReceiver;
     }
 
     /// @notice Called with the sale price to determine how much royalty
