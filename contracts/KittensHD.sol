ef6bfb61a262642ee8b4a6794d1307f7fbe32d83
Gabriel R. Abreu <grsabreu@gmail.com>
2021-11-20 20:27:15 -0300

    feat: honorary claim

--- contracts/KittensHD.sol
+++ contracts/KittensHD.sol
@@ -7,105 +7,138 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./FantomKittens.sol";
+import "./HonoraryKittens.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-contract KittensHD is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract KittensHD is
+  ERC721,
+  ERC721Enumerable,
+  ERC721URIStorage,
+  Ownable,
+  IERC2981
+{
+  using Counters for Counters.Counter;
 
-    Counters.Counter private _tokenIdCounter = Counters.Counter(687);
-    Counters.Counter private _generalMintCounter;
+  Counters.Counter private _tokenIdCounter = Counters.Counter(687);
+  Counters.Counter private _generalMintCounter;
 
-    address payable public depositAddress =
-        payable(0xC748E6dE30222F4e9bC01812860FF005A82543E6);
-    uint256 public maxMintable = 10_000;
-    string public baseURI = "https://kittens.fakeworms.studio/api/kitten-hd/";
+  address payable public depositAddress =
+    payable(0xC748E6dE30222F4e9bC01812860FF005A82543E6);
+  uint256 public maxMintable = 10_000;
+  string public baseURI = "https://kittens.fakeworms.studio/api/kitten-hd/";
 
-    FantomKittens fantomKittens =
-        FantomKittens(0xfD211f3B016a75bC8d73550aC5AdC2f1cAE780C0);
+  uint256 public royaltiesPercentage = 7;
 
-    constructor() ERC721("KittensHD", "KITTENHD") {}
+  FantomKittens fantomKittens =
+    FantomKittens(0xfD211f3B016a75bC8d73550aC5AdC2f1cAE780C0);
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "https://kittens.fakeworms.studio/api/kitten/";
+  HonoraryKittens honoraryKittens =
+    HonoraryKittens(0xE65469083B4f50d1EcD089584c671Bb1d23F9AC7);
+
+  constructor() ERC721("KittensHD", "KITTENHD") {}
+
+  function _baseURI() internal pure override returns (string memory) {
+    return "https://kittens.fakeworms.studio/api/kitten/";
+  }
+
+  function setDepositAddress(address payable to) public onlyOwner {
+    depositAddress = to;
+  }
+
+  function setRoyaltiesPercentage(uint256 newPercentage) public onlyOwner {
+    royaltiesPercentage = newPercentage;
+  }
+
+  function setBaseURI(string memory newURI) public onlyOwner {
+    baseURI = newURI;
+  }
+
+  // get price based on quantity
+  function getPrice(uint256 quantity) public pure returns (uint256) {
+    if (quantity >= 10) {
+      return 4.0 ether;
+    } else if (quantity >= 3) {
+      return 4.1 ether;
+    } else {
+      return 4.2 ether;
     }
+  }
 
-    function setDepositAddress(address payable to) public onlyOwner {
-        depositAddress = to;
+  function claim(uint256 quantity) public payable {
+    uint256 id = _generalMintCounter.current();
+
+    require(quantity > 0, "Invalid amount");
+
+    _generalMintCounter.increment();
+    // 4.2
+    uint256 price = 4.2 ether;
+
+    require(msg.value == price, "Invalid amount");
+    require(id <= maxMintable, "No more kittens are available");
+
+    // transfer amount to owner
+    depositAddress.transfer(price);
+
+    _safeMint(msg.sender, id);
+  }
+
+  function ogClaim() public {
+    for (uint256 i = 0; i < fantomKittens.balanceOf(msg.sender); i++) {
+      uint256 id = fantomKittens.tokenOfOwnerByIndex(msg.sender, i);
+
+      _safeMint(msg.sender, id);
     }
+  }
 
-    function setBaseURI(string memory newURI) public onlyOwner {
-        baseURI = newURI;
+  function honoraryClaim() public {
+    for (uint256 i = 0; i < honoraryKittens.balanceOf(msg.sender); i++) {
+      uint256 id = honoraryKittens.tokenOfOwnerByIndex(msg.sender, i);
+
+      _safeMint(msg.sender, id);
     }
+  }
 
-    // get price based on quantity
-    function getPrice(uint256 quantity) public pure returns (uint256) {
-        if (quantity >= 10) {
-            return 4.0 ether;
-        } else if (quantity >= 3) {
-            return 4.1 ether;
-        } else {
-            return 4.2 ether;
-        }
-    }
+  // The following functions are overrides required by Solidity.
 
-    function claim(uint256 quantity) public payable {
-        uint256 id = _generalMintCounter.current();
+  function _beforeTokenTransfer(
+    address from,
+    address to,
+    uint256 tokenId
+  ) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
 
-        require(quantity > 0, "Invalid amount");
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
 
-        _generalMintCounter.increment();
-        // 4.2
-        uint256 price = 4.2 ether;
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
 
-        require(msg.value == price, "Invalid amount");
-        require(id <= maxMintable, "No more kittens are available");
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable, IERC165)
+    returns (bool)
+  {
+    return
+      type(IERC2981).interfaceId == interfaceId ||
+      super.supportsInterface(interfaceId);
+  }
 
-        // transfer amount to owner
-        depositAddress.transfer(price);
-
-        _safeMint(msg.sender, id);
-    }
-
-    function ogClaim() public {
-        // iterate over tokenOfOwnerByIndex from 0 to 419 for fantomKittens
-        for (uint256 i = 0; i < fantomKittens.balanceOf(msg.sender); i++) {
-            uint256 id = fantomKittens.tokenOfOwnerByIndex(msg.sender, i);
-
-            _safeMint(msg.sender, id);
-        }
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override(ERC721, ERC721Enumerable) {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId)
-        internal
-        override(ERC721, ERC721URIStorage)
-    {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function royaltyInfo(uint256 tokenId, uint256 _salePrice)
+    external
+    view
+    override(IERC2981)
+    returns (address receiver, uint256 royaltyAmount)
+  {
+    uint256 _royalties = (_salePrice * (royaltiesPercentage / 100));
+    return (depositAddress, _royalties);
+  }
 }
