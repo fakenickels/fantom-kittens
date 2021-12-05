219c1eab115387836e527de976174daff7edbe22
Gabriel R. Abreu <grsabreu@gmail.com>
2021-12-05 17:47:12 -0300

    feat(claim): finish merkle distributor

--- contracts/KittenHDRKittenMerkleDistributor.sol
+++ contracts/KittenHDRKittenMerkleDistributor.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./KittensHD.sol";
 
 // Allows anyone to claim a token if they exist in a merkle root.
 interface IMerkleDistributor {
   // Returns the address of the token distributed by this contract.
-  function rKittenAddress() external view returns (address);
+  function kittensHDAddress() external view returns (address);
 
   // Returns the merkle root of the merkle tree containing account balances available to claim.
   function merkleRoot() external view returns (bytes32);
@@ -26,15 +26,18 @@ interface IMerkleDistributor {
   event Claimed(uint256 index, address account, uint256 amount);
 }
 
-contract KittenHDRKittenMerkleDistributor is IMerkleDistributor {
-  address public immutable override rKittenAddress;
+contract KittenHDRKittenMerkleDistributor is
+  IMerkleDistributor,
+  IERC721Receiver
+{
+  address public immutable override kittensHDAddress;
   bytes32 public immutable override merkleRoot;
 
   // This is a packed array of booleans.
   mapping(uint256 => uint256) private claimedBitMap;
 
   constructor(address token_, bytes32 merkleRoot_) {
-    rKittenAddress = token_;
+    kittensHDAddress = token_;
     merkleRoot = merkleRoot_;
   }
 
@@ -71,20 +74,60 @@ contract KittenHDRKittenMerkleDistributor is IMerkleDistributor {
     // Mark it claimed and send the token.
     _setClaimed(index);
 
-    uint256 currentTokenId = KittensHD(rKittenAddress).getRKittenClaimCounter();
+    KittensHD kittensHD = KittensHD(kittensHDAddress);
+    uint256 currentTokenId = kittensHD.getRKittenClaimCounter();
 
-    // claim NFTs
-    KittensHD(rKittenAddress).daoRKITTENClaim(amount);
+    if (currentTokenId + amount > 1020) {
+      uint256 amountToMint = 1020 - currentTokenId;
+      uint256 surplus = amount - amountToMint;
+      // claim NFTs
+      kittensHD.daoRKITTENClaim(amountToMint);
 
-    // transfer rkitten amount back to sender from address(this)
-    for (uint256 i = 0; i < amount; i++) {
-      KittensHD(rKittenAddress).safeTransferFrom(
-        address(this),
-        msg.sender,
-        currentTokenId + i
-      );
+      // transfer rkitten amount back to sender from address(this)
+      for (uint256 i = 0; i < amountToMint; i++) {
+        kittensHD.safeTransferFrom(
+          address(this),
+          msg.sender,
+          currentTokenId + i
+        );
+      }
+
+      // mint surplus from daoAnyClaim
+      uint256 generalCurrentId = kittensHD.getGeneralMintCounter();
+      kittensHD.unpauseMinting();
+      kittensHD.daoAnyClaim(surplus);
+      // transfer rkitten amount back to sender from address(this)
+      for (uint256 i = 0; i < surplus; i++) {
+        kittensHD.safeTransferFrom(
+          address(this),
+          msg.sender,
+          generalCurrentId + i
+        );
+      }
+      kittensHD.pauseMinting();
+    } else {
+      // claim NFTs
+      kittensHD.daoRKITTENClaim(amount);
+
+      // transfer rkitten amount back to sender from address(this)
+      for (uint256 i = 0; i < amount; i++) {
+        kittensHD.safeTransferFrom(
+          address(this),
+          msg.sender,
+          currentTokenId + i
+        );
+      }
     }
 
     emit Claimed(index, msg.sender, amount);
+  }
+
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external override returns (bytes4) {
+    return this.onERC721Received.selector;
   }
 }
