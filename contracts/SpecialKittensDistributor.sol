137a263aba3b2a3f13cf9b112f324c1d6fc14697
Gabriel R. Abreu <grsabreu@gmail.com>
2021-12-06 18:48:03 -0300

    feat(kittens-hd): special kittens distributor and tests

--- contracts/SpecialKittensDistributor.sol
+++ contracts/SpecialKittensDistributor.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "./HonoraryKittens.sol";
 import "./KittensHD.sol";
 
-contract SpecialKittensDistributor is Ownable {
+contract SpecialKittensDistributor is Ownable, IERC721Receiver {
   HonoraryKittens honoraryKittens;
 
   KittensHD kittensHD;
 
-  mapping(address => bool) public claims;
+  mapping(uint256 => bool) public claimBitmap;
 
   constructor(address kittensHDAddress, address honoraryKittensAddress) {
     kittensHD = KittensHD(kittensHDAddress);
@@ -17,26 +18,49 @@ contract SpecialKittensDistributor is Ownable {
   }
 
   function claim() public {
-    require(honoraryKittens.balanceOf(msg.sender) > 0);
-    require(!claims[msg.sender], "You've already claimed");
+    require(
+      honoraryKittens.balanceOf(msg.sender) > 0,
+      "You don't have any honorary kittens"
+    );
 
     // check if the sender already minted a kitten hd due to the KittensHD contract bug
-    uint256 index = 0;
-    while (true) {
+
+    // for of kittenHD.balance of msg.sender
+    for (uint256 index = 0; index < kittensHD.balanceOf(msg.sender); index++) {
       uint256 tokenId_ = kittensHD.tokenOfOwnerByIndex(msg.sender, index);
       require(
         // check if the honorary holder has already minted within the reserved range
-        tokenId_ < 667 && tokenId_ > 687,
+        tokenId_ < 667 || tokenId_ > 687,
         "You've already claimed your free HD"
       );
-      index++;
     }
 
-    claims[msg.sender] = true;
+    // check if honorary kitten was already "spent"
+    for (
+      uint256 index = 0;
+      index < honoraryKittens.balanceOf(msg.sender);
+      index++
+    ) {
+      uint256 tokenId_ = honoraryKittens.tokenOfOwnerByIndex(msg.sender, index);
+      require(!claimBitmap[tokenId_], "You've already claimed this token");
+      claimBitmap[tokenId_] = true;
+    }
 
     // call daoClaim from KittensHD and transfer to msg.sender
     uint256 tokenId = kittensHD.getGeneralMintCounter();
+
+    kittensHD.unpauseMinting();
     kittensHD.daoAnyClaim(1);
     kittensHD.safeTransferFrom(address(this), msg.sender, tokenId);
+    kittensHD.pauseMinting();
+  }
+
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external override returns (bytes4) {
+    return this.onERC721Received.selector;
   }
 }
--- scripts/deploy_special_kitten_distributor.js
+++ scripts/deploy_special_kitten_distributor.js
@@ -8,20 +8,32 @@ async function main() {
   const Contract = await hre.ethers.getContractFactory(
     "SpecialKittensDistributor"
   );
-  const contract = await Contract.deploy(
+  const args = [
     // KittensHD
     "0xad956DF38D04A9A555E079Cf5f3fA59CB0a25DC9",
     // Kittens Specials
-    "0xE65469083B4f50d1EcD089584c671Bb1d23F9AC7"
-  );
+    "0xE65469083B4f50d1EcD089584c671Bb1d23F9AC7",
+  ];
+  const contract = await Contract.deploy(...args);
   await contract.deployed();
 
   console.log("Deployed to:", contract.address);
+
+  // get KittensHD contract and call grantDAOMemberRole for contract.address
+  const kittensHDContract = await ethers.getContractAt(
+    "KittensHD",
+    kittensHDAddress,
+    deployer
+  );
+
+  console.log("Granting role...");
+  await kittensHDContract.grantDAOMemberRole(contract.address);
 
   if (process.env.CHAIN_SCAN_TOKEN) {
     console.log("Verifying ze contract");
     await hre.run("verify:verify", {
       address: contract.address,
+      constructorArguments: args,
     });
   }
 }
